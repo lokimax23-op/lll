ae673bdba99830ee6a35e1d961acbc28d398f252
lokimax23-op <lokimax84@gmail.com>
2026-08-28 10:14:24 +0100

    modified:   classproject/settings.py 	deleted:    requirement.txt 	modified:   requirements.txt

--- classproject/settings.py
+++ classproject/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 from pathlib import Path
-from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(0f^--+b4h(j@bjdsn0-^=^(8j4l7w6ock21x6((pkiqc%+jn7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG =  True
 ALLOWED_HOSTS = (
     os.getenv('ALLOWED_HOSTS')
     or os.getenv('RENDER_EXTERNAL_HOSTNAME')
